9ebb9f5c9e2ffd312f84f7fa4c28c966cc8f3755
ade-mic <ademic.aina@gmail.com>
2024-06-17 15:36:40 +0100

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,32 +1,7 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-  
-   return {
-      next() {
-        if (currentDepartmentIndex >= departments.length) {
-          return { done: true };
-        }
-  
-        const currentDepartment = departments[currentDepartmentIndex];
-        const employeesInDepartment = allEmployees[currentDepartment];
-  
-        if (currentEmployeeIndex >= employeesInDepartment.length) {
-          currentDepartmentIndex++;
-          currentEmployeeIndex = 0;
-          return this.next(); // Recurse to the next department
-        }
-  
-        const currentEmployee = employeesInDepartment[currentEmployeeIndex];
-        currentEmployeeIndex++;
-  
-        return { value: currentEmployee, done: false };
-      },
-      Symbol.iterator {
-        return this;
-      },
-    };
+  const allEmployees = [];
+  for (const dept of Object.values(report.allEmployees)) {
+    allEmployees.push(...dept);
   }
-  
+  return allEmployees[Symbol.iterator]();
+}
