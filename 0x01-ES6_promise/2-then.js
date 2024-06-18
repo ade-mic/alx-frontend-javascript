f986573b01842a9a3e525dbbd3ea94b716047f7f
ade-mic <ademic.aina@gmail.com>
2024-06-18 20:34:49 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,10 @@ export default function hansleResponseFromAPI(promise) {
   promise
     .then(() => {
       const data = { status: 200, body: 'success' };
+      console.log('Got a response from the API');
       return (data);
     })
-    .catch(() => new Error())
+    .catch(() => Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
