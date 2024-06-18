46951ab30392abf5eb5da6378e31244eb01c7f1c
ade-mic <ademic.aina@gmail.com>
2024-06-18 22:30:18 +0100

    check again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const userResult = await signUpUser(firstName, lastName);
+  const photoResult = await uploadPhoto(fileName);
+
+  return [
+    { status: userResult.status, value: userResult.value },
+    { status: photoResult.status, value: photoResult.value },
+  ];
 }
+
+export default handleProfileSignup;
