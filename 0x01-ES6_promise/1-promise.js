c6d71c7c68520fa9b1166b4aaec1fc1efbb124a3
ade-mic <ademic.aina@gmail.com>
2024-06-18 19:56:46 +0100

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      const data = { status: 200, body: 'Success'};
+      resolve(data);
+    } else {
+      const errorMessage = 'The fake API is not working currently'
+      reject(new Error(errorMessage));
+    }
+  });
+}
