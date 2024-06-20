ad9ff4061c6ce0473a1b39e474bbc3bae0fbcd47
ade-mic <ademic.aina@gmail.com>
2024-06-20 13:51:18 +0100

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -41,4 +41,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
