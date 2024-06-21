67e6cef4d774a5601a69a8b52c0382c728456e72
ade-mic <ademic.aina@gmail.com>
2024-06-21 15:08:28 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
+// 8-clean_set.js
+
 export default function cleanSet(set, startString) {
-  if (startString !== '') {
-    const filteredValues = [...set].map((value) => {
-      if (value.startsWith(startString)) {
-        const newValue = value.substr(startString.length);
-        return newValue;
-      }
-      return '';
-    });
-    const hyphenated = filteredValues.join('-');
-    return hyphenated.replace(/-$/, '');
+  if (!startString || typeof startString !== 'string' || startString.length === 0) {
+    return '';
   }
-  return '';
+
+  const filteredValues = [];
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      filteredValues.push(value.slice(startString.length));
+    }
+  }
+
+  return filteredValues.join('-');
 }
