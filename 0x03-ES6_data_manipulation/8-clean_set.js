e27fa4b8779f41c4abe2b311beee69c5bef64405
ade-mic <ademic.aina@gmail.com>
2024-06-21 15:55:36 +0100

    88-clean_set.js rewritten for incorrect input

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,12 @@
-// 8-clean_set.js
-
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || startString.length === 0) {
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
   const filteredValues = [];
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       filteredValues.push(value.slice(startString.length));
     }
   }
